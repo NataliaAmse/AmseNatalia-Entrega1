1ca70e3bdc2187b186c6abe8e346368abc5da07e
Natalia Amse <natalia.amse@gmail.com>
2023-12-09 14:16:30 -0300

    entrega3

--- JS/index.js
+++ JS/index.js
@@ -1,99 +1,86 @@
-let nombreIngresado = prompt("Ingresa tu nombre:");
-let apellidoIngresado = prompt("Ingresa tu apellido:");
-
-function saludar(nombre, apellido) {
-    alert("Bienvenido " + nombre + " " + apellido);
-}
-
-saludar(nombreIngresado, apellidoIngresado);
-
-
-
-let destino = prompt("¿A dónde queres viajar? Bariloche (1), Mendoza (2), Cataratas (3) y El Calafate (4)");
-
-switch (destino) {
-    case "1":
-        alert("Elegiste de destino a Bariloche. El precio del paquete es de $300000");
-        break;
-    case "2":
-        alert("Elegiste de destino a Mendoza. El precio del paquete es de $200000");
-        break;
-    case "3":
-        alert("Elegiste de destino a Cataratas. El precio del paquete es de $350000");
-        break;
-    case "4":
-        alert("Elegiste de destino a El Calafate. El precio del paquete es de $500000");
-        break;
-    default:
-        alert("opción incorrecta. Por favor elegir opción 1, 2, 3 o 4");
-        break;
-}
-
-
-let descuento = "1";
-let precioDeLista = "2";
-
-let pago = prompt("Si abonas con efectivo tenes un 10% de descuento. Si vas a abonar con efectivo responde: 1, si no vas a abonar con efectivo responde 2");
-
-while (pago !== "1" && pago !== "2") {
-    alert("Introducí las opciones 1 o 2 unicamente");
-    pago = prompt("Si abonas con efectivo tenes un 10% de descuento. Si vas a abonar con efectivo responde: 1, si no vas a abonar con efectivo responde 2");
-}
-
-if (pago === "1") {
-    (alert("Vas a abonar un 10% menos en efectivo"));
-} else if (pago === "2") {
-    (alert("Abonando con tarjeta o transferencia es el mismo precio"));
-} else {
-    alert("Introducí las opciones 1 o 2 unicamente");
-}
-
-
 const DESTINOS = [
-    { paquete: "Bariloche", precio: 300000 },
-    { paquete: "Mendoza", precio: 200000 },
-    { paquete: "Cataratas", precio: 350000 },
-    { paquete: "El Calafate", precio: 500000 }
-]
+    { id: 1, paquete: "Bariloche", precio: 300000 },
+    { id: 2, paquete: "Mendoza", precio: 200000 },
+    { id: 3, paquete: "Cataratas", precio: 350000 },
+    { id: 4, paquete: "El Calafate", precio: 500000 }
+];
 
-function bonificacion(destino) {
-    const DESCUENTO = 0.1;
-    const PAQUETE_CON_DESC = destino.precio * (1 - DESCUENTO);
-    alert(`El paquete ${destino.paquete}, con el descuento en efectivo te queda en: ${PAQUETE_CON_DESC}`)
+function agregarAlCarrito(id, paquete, precio) {
+    const CARRITO = JSON.parse(localStorage.getItem(`carrito`)) || [];
+    CARRITO.push({ id, paquete, precio });
+    localStorage.setItem(`carrito`, JSON.stringify(CARRITO));
+    verCarrito();
 };
 
-DESTINOS.forEach(bonificacion);
+function verCarrito() {
+    const CARRITO = JSON.parse(localStorage.getItem('carrito')) || [];
+    const VIAJES = document.getElementById('viajes');
+    const totalElement = document.getElementById('total');
+    let total = 0;
+
+    VIAJES.innerHTML = '';
+
+    CARRITO.forEach(item => {
+        const LISTADO_DESTINOS = document.createElement('li');
+        LISTADO_DESTINOS.innerHTML = `
+            <span>${item.paquete}</span>
+            <span>${item.precio}</span>
+            <button onclick="eliminarDelCarrito(${item.id})">Eliminar</button>
+        `;
+        VIAJES.appendChild(LISTADO_DESTINOS);
+        total += item.precio;
+    });
+
+    totalElement.textContent = total;
+}
+
+function eliminarDelCarrito(id) {
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    carrito = carrito.filter(item => item.id !== id);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    verCarrito();
+}
+
 
 
 class Destino {
-    constructor(paquete, precio, img) {
+    constructor(id, paquete, precio, img) {
+        this.id = id;
         this.paquete = paquete;
         this.precio = precio;
         this.img = img;
     }
 }
 
-const BARILOCHE = new Destino("Bariloche", 300000, "./src/bariloche.png");
-const MENDOZA = new Destino("Mendoza", 200000, "./src/mendoza.jpg");
-const CATARATAS = new Destino("Cataratas", 350000, "./src/cataratas.jpg");
-const ELCALAFATE = new Destino("El Calafate", 500000, "./src/calafate.jpg");
+function tarjetaDestinos() {
+    const CONTENEDOR_DESTINOS = document.getElementById(`contenedorDestinos`);
+
+
+const BARILOCHE = new Destino(1,"Bariloche", 300000, "./src/bariloche.png");
+const MENDOZA = new Destino(2,"Mendoza", 200000, "./src/mendoza.jpg");
+const CATARATAS = new Destino(3,"Cataratas", 350000, "./src/cataratas.jpg");
+const ELCALAFATE = new Destino(4,"El Calafate", 500000, "./src/calafate.jpg");
 
 const ArrayDestinos = [BARILOCHE, MENDOZA, CATARATAS, ELCALAFATE];
 
-ArrayDestinos.forEach(Destino => {
-    let div = document.createElement("div");
-    div.className = "card";
-    div.innerHTML = `<div class="card" style="width: 20rem;">
-        <img src="${Destino.img}" class="card-img-top" alt="...">
+ArrayDestinos.forEach(destino => {
+    const DIV = document.createElement(`div`);
+    DIV.className = "card";
+    DIV.innerHTML = `<div class="card" style="width: 20rem;">
+        <img src="${destino.img}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${Destino.paquete}</h5>
-          <h5 class="card-title">$${Destino.precio}</h5>
+          <h3 class="card-title">${destino.paquete}</h3>
+          <h3 class="card-title">${destino.precio}</h3>
           <p class="card-text">Disfruta tu próximo destino.</p>
-          <a href="#" class="btn btn-primary">Agregar al carrito</a>
+          <button onclick="agregarAlCarrito(${destino.id}, '${destino.paquete}', ${destino.precio})" class="btn btn-primary">Agregar al carrito</button>
+
         </div>
       </div>
-         `
-    contenedorDestinos.appendChild(div);
-})
+         `;
+    CONTENEDOR_DESTINOS.appendChild(DIV);
 
+});
+}
 
+verCarrito();
+tarjetaDestinos();
