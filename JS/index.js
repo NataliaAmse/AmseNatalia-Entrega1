d7cbbdaff5836d65689c8bc6ba1ceca6973b1830
Natalia Amse <natalia.amse@gmail.com>
2023-12-25 18:35:25 -0300

    ultimos commits

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,50 @@
+const PROMOCION = document.getElementById("promocion");
+PROMOCION.addEventListener("click", () => {
+    Swal.fire({
+        text: "Si abonas en efectivo en nuestras sucursales vas a obtener un 10% de descuento",
+        confirmButtonText: "Aceptar",
+        confirmButtonColor: "grey",
+        width: 600,
+        padding: "3em",
+        color: "grey",
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("./src/avion.gif")
+          right center
+          no-repeat
+        `
+    })
+
+})
+
+
+const COTIZADOR = "https://criptoya.com/api/dolar";
+
+const COTIZACION = document.getElementById("cotizacion");
+
+setInterval(()=>{
+    fetch(COTIZADOR)
+    .then(response => response.json())
+    .then(({blue,oficial,qatar})=>{
+        COTIZACION.innerHTML = `
+        <h3>Tipos de cambio del día: </h3>
+        <p>Dolar Oficial: ${oficial}</p>
+        <p>Dolar Blue: ${blue}</p>
+        <p>Dolar Qatar: ${qatar}</p>
+        `
+    })
+    .catch(error => console.log(error))
+
+
+},100);
+
+
+
 const DESTINOS = [
-    { id: 1, paquete: "Bariloche", precio: 300000 },
-    { id: 2, paquete: "Mendoza", precio: 200000 },
-    { id: 3, paquete: "Cataratas", precio: 350000 },
-    { id: 4, paquete: "El Calafate", precio: 500000 }
+    { id: 1, paquete: "Bariloche", precio: 300 },
+    { id: 2, paquete: "Mendoza", precio: 200 },
+    { id: 3, paquete: "Cataratas", precio: 350 },
+    { id: 4, paquete: "El Calafate", precio: 500 }
 ];
 
 function agregarAlCarrito(id, paquete, precio) {
@@ -56,31 +98,36 @@ function tarjetaDestinos() {
     const CONTENEDOR_DESTINOS = document.getElementById(`contenedorDestinos`);
 
 
-const BARILOCHE = new Destino(1,"Bariloche", 300000, "./src/bariloche.png");
-const MENDOZA = new Destino(2,"Mendoza", 200000, "./src/mendoza.jpg");
-const CATARATAS = new Destino(3,"Cataratas", 350000, "./src/cataratas.jpg");
-const ELCALAFATE = new Destino(4,"El Calafate", 500000, "./src/calafate.jpg");
+    const BARILOCHE = new Destino(1, "Bariloche", 300, "./src/bariloche.png");
+    const MENDOZA = new Destino(2, "Mendoza", 200, "./src/mendoza.jpg");
+    const CATARATAS = new Destino(3, "Cataratas", 350, "./src/cataratas.jpg");
+    const ELCALAFATE = new Destino(4, "El Calafate", 500, "./src/calafate.jpg");
 
-const ArrayDestinos = [BARILOCHE, MENDOZA, CATARATAS, ELCALAFATE];
+    const ArrayDestinos = [BARILOCHE, MENDOZA, CATARATAS, ELCALAFATE];
 
-ArrayDestinos.forEach(destino => {
-    const DIV = document.createElement(`div`);
-    DIV.className = "card";
-    DIV.innerHTML = `<div class="card" style="width: 20rem;">
+    ArrayDestinos.forEach(destino => {
+        const DIV = document.createElement(`div`);
+        DIV.className = "card";
+        DIV.innerHTML = `<div class="card" style="width: 22rem;">
         <img src="${destino.img}" class="card-img-top" alt="...">
         <div class="card-body">
           <h3 class="card-title">${destino.paquete}</h3>
           <h3 class="card-title">${destino.precio}</h3>
-          <p class="card-text">Disfruta tu próximo destino.</p>
+          <p class="card-text">Próxima salida el 20/04/2024.</p>
+          <p class="card-text">Precios expresados en USD a la cotización del día del dolar oficial.</p>
           <button onclick="agregarAlCarrito(${destino.id}, '${destino.paquete}', ${destino.precio})" class="btn btn-primary">Agregar al carrito</button>
 
         </div>
       </div>
          `;
-    CONTENEDOR_DESTINOS.appendChild(DIV);
+        CONTENEDOR_DESTINOS.appendChild(DIV);
 
-});
+    });
+
+
+    
 }
 
 verCarrito();
 tarjetaDestinos();
+
